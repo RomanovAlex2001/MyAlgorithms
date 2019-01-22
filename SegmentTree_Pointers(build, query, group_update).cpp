95a0fbfbb1ce4443e6cfb1bcd14e4616f2e11f69
Alex Romanov <ea1221@mail.ru>
2019-01-22 19:33:36 +0300

    Create SegmentTree_Pointers(build, query, group_update).cpp

---
+++ SegmentTree_Pointers(build, query, group_update).cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+const int INF = 1e9 + 7;
+
+struct node {
+    int left, right;
+    int maxi, add;
+    node * child_left;
+    node * child_right;
+};
+
+node * build(vector<int> & A, int left, int right) {
+    if (left > right) {
+        return nullptr;
+    }
+    node * res = new node;
+    res->left = left;
+    res->right = right;
+    res->add = 0;
+    if (left == right) {
+        res->child_left = NULL;
+        res->child_right = NULL;
+        res->maxi = A[left];
+    } else {
+        int mid = (left + right) / 2;
+        res->child_left = build(A, left, mid);
+        res->child_right = build(A, mid + 1, right);
+        res->maxi = max(res->child_left->maxi, res->child_right->maxi);
+    }
+    return res;
+}
+
+int query(node * root, int left, int right) {
+    if (right < root->left || left > root->right) {
+        return -INF;
+    }
+    if (right >= root-> right && left <= root->left) {
+        return root->maxi + root->add;
+    }
+    int ans1 = query(root->child_left, left, right);
+    int ans2 = query(root->child_right, left, right);
+    return max(ans1, ans2) + root -> add;
+}
+
+void update(node * root, int idx, int val) {
+    if (idx < root->left || idx > root->right) {
+        return;
+    }
+    if (root->left == root->right) {
+        root->maxi = val;
+        return;
+    }
+    update(root->child_left, idx, val);
+    update(root->child_right, idx, val);
+    root->maxi = max(root->child_left->maxi, root->child_right->maxi);
+}
+
+void group_update(node * root, int left, int right, int delta) {
+    if (right < root->left || left > root->right) {
+        return;
+    }
+    if (right >= root-> right && left <= root->left) {
+        root->add += delta;
+        return;
+    }
+    group_update(root->child_left, left, right, delta);
+    group_update(root->child_right, left, right, delta);
+    root->maxi = max(root->child_left->maxi + root->child_left->add,
+                     root->child_right->maxi + root->child_right->add);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> A(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> A[i];
+    }
+    node * root = build(A, 0, n - 1);
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; ++i) {
+        char c;
+        cin >> c;
+        if (c == 'u') {
+            int l, r;
+            cin >> l >> r;
+            l--;
+            A[l] = r;
+            update(root, l, r);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            l--; r--;
+            int ans1 = query(root, l, r);
+            int ans2 = -INF;
+            for (int q = l; q <= r; q++) {
+                if (ans2 < A[q]) {
+                    ans2 = A[q];
+                }
+            }
+            if (ans1 == ans2) {
+                cout << "OK " << ans1 << endl;
+            } else {
+                cout << "WA" << endl;
+            }
+        }
+    }
+    return 0;
+}
